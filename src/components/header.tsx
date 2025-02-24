7830012904d01f9b970c9a623f30360dd7906551
gruv0o <renaultremi51100@gmail.com>
2025-02-24 20:56:26 +0100

    ~ sidebar.tsx + Header.tsx
    Rendu des sidebar et header fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const HeaderComp = () => {
     return (
-        <header className="w-full bg-gray-700 shadow-md justify-between h-20 pt-1">
+        <header className="w-full bg-gray-700 shadow-md justify-between h-20 pt-1 fixed">
             <div className="text-xl font-bold text-gray-900 mb-0">
                 <Link href="/">
                     <span className="cursor-pointer">
